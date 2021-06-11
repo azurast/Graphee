5fe65ee0b1aa44c3b299336e10f4317264d01dac
Azura Sakan Taufik <azstima@gmail.com>
2021-06-11 15:05:25 +0700

    add core data entity & relationship and generate subclass

---
+++ Graphee/Utilities/Core Data/Core Data Class/Folder+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Folder+CoreDataClass.swift
+//  Graphee
+//
+//  Created by Azura on 11/06/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Folder)
+public class Folder: NSManagedObject {
+
+}
---
+++ Graphee/Utilities/Core Data/Core Data Class/Photo+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  Photo+CoreDataClass.swift
+//  Graphee
+//
+//  Created by Azura on 11/06/21.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(Photo)
+public class Photo: NSManagedObject {
+
+}
