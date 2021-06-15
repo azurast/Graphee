ceaad73898700acd1d090a2bba740ffe1efd90b6
Luis Genesius <luis.huang321@gmail.com>
2021-06-15 08:49:37 +0700

    Modify PreviewViewController

--- Graphee/Features/MainCamera/Controllers/PreviewViewController.swift
+++ Graphee/Features/MainCamera/Controllers/PreviewViewController.swift
@@ -9,21 +9,90 @@ import UIKit
 
 class PreviewViewController: UIViewController {
 
+    private var topView: UIView!
+    private var directionLabel: UILabel!
+    private var photoImageView: UIImageView!
+    private var bottomView: BottomCameraView!
+    
+    
+
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        view.backgroundColor = .black
+        
+        let window = UIApplication.shared.windows.filter {$0.isKeyWindow}.first
+        let height = window?.windowScene?.statusBarManager?.statusBarFrame.height ?? 0
+        
+        setupNavigationView(statusBarHeight: height)
+        setupBottomView()
+        setupImageView(statusBarHeight: height)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupNavigationView(statusBarHeight: CGFloat) {
+        
+        topView = UIView(frame: CGRect(x: 0, y:  statusBarHeight, width: view.frame.width, height: 80))
+        topView.backgroundColor = .black
+        view.addSubview(topView)
+        
+        directionLabel = UILabel()
+        directionLabel = UILabel(frame: CGRect(x: 0, y: 0, width: topView.frame.width, height: 0))
+        directionLabel.text = CameraImages.shared.getNextDirectionInString()
+        directionLabel.textColor = .white
+        directionLabel.numberOfLines = 0
+        directionLabel.frame.size.height = directionLabel.intrinsicContentSize.height
+        directionLabel.textAlignment = .center
+        directionLabel.center = CGPoint(x: topView.frame.width / 2, y: topView.frame.height - 30)
+        topView.addSubview(directionLabel)
     }
-    */
 
+    private func setupBottomView() {
+        let veryBottomView = UIView(frame: CGRect(x: 0, y: view.frame.height - 50, width: view.frame.width, height: 50))
+        veryBottomView.backgroundColor = .black
+        view.addSubview(veryBottomView)
+        
+        bottomView = BottomCameraView(frame: CGRect(x: 0, y: view.frame.height - 150 - 50, width: view.frame.width, height: 150))
+        bottomView.createPreviewButton()
+        bottomView.previewButtonDelegate = self
+        bottomView.backgroundColor = .black.withAlphaComponent(0.5)
+        view.addSubview(bottomView)
+        
+        if CameraImages.shared.isAllDictImageAvailable() {
+            bottomView.hideDoneButton()
+        }
+    }
+    
+    private func setupImageView(statusBarHeight: CGFloat) {
+        if CameraImages.shared.returnRatioFromDirection(direction: CameraImages.shared.getNextDirectionInString()) == SettingHelper.shared.ratio169 {
+            photoImageView = UIImageView(frame: CGRect(x: 0, y: topView.frame.height + statusBarHeight, width: view.frame.width, height: view.frame.height - topView.frame.height - statusBarHeight - 50))
+        } else if CameraImages.shared.returnRatioFromDirection(direction: CameraImages.shared.getNextDirectionInString()) == SettingHelper.shared.ratio43 {
+            photoImageView = UIImageView(frame: CGRect(x: 0, y: topView.frame.height + statusBarHeight + 50, width: view.frame.width, height: view.frame.height - 50 - bottomView.frame.height - topView.frame.height - statusBarHeight - 50))
+        } else {
+            photoImageView = UIImageView(frame: CGRect(x: 0, y: topView.frame.height + statusBarHeight + 50, width: view.frame.width, height: view.frame.width))
+        }
+        
+        photoImageView.image = CameraImages.shared.returnImageFromDirection(direction: CameraImages.shared.getNextDirectionInString())
+        
+        photoImageView.contentMode = .scaleAspectFit
+        view.addSubview(photoImageView)
+//        view.bringSubviewToFront(topView)
+        view.bringSubviewToFront(bottomView)
+    }
+
+}
+
+extension PreviewViewController: PreviewButtonDelegate {
+    func previewDoneButtonTapped() {
+        print(CameraImages.shared.isAllDictImageAvailable())
+        if !CameraImages.shared.isAllDictImageAvailable() {
+            CameraImages.shared.configureTheNextDirection()
+            self.navigationController?.popViewController(animated: true)
+        }
+    }
+    
+    func previewRetakeButtonTapped() {
+        self.navigationController?.popViewController(animated: true)
+    }
 }
