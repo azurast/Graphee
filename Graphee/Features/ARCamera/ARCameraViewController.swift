e9a5ae8865bbf6e7cc0996a820bb5a5dde4e9ecb
Azura <azstima@gmail.com>
2021-06-16 18:59:08 +0700

    add label

--- Graphee/Features/ARCamera/ARCameraViewController.swift
+++ Graphee/Features/ARCamera/ARCameraViewController.swift
@@ -7,14 +7,20 @@
 
 import UIKit
 import ARKit
+import RealityKit
 
-class ARCameraViewController: UIViewController {
+class ARCameraViewController: UIViewController, UIActionSheetDelegate {
 
     @IBOutlet weak var sceneView: ARSCNView!
+    @IBOutlet weak var statusPanel: UIVisualEffectView!
+    @IBOutlet weak var statusLabel: UILabel!
+    
+    var referencePoint : SCNNode!
     var hasBeenPlaced: Bool! = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        statusPanel.isHidden = false
         // Add Replace Button
         self.navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Replace", style: .plain, target: self, action: #selector(replacePoint))
         // Add tap gesture recognizer
@@ -26,6 +32,13 @@ class ARCameraViewController: UIViewController {
     
     @objc func replacePoint() {
         print("Replace Point Tapped")
+        let actionSheet = UIAlertController(title: "Replace current reference point?", message: "You will lose current point", preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Replace", style: .destructive, handler: {_ in
+            self.referencePoint?.removeFromParentNode()
+            self.hasBeenPlaced = false
+        }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(actionSheet, animated: true, completion: nil)
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -55,40 +68,45 @@ class ARCameraViewController: UIViewController {
     
     @objc func addReferencePoint(withGestureRecognizer recognizer: UIGestureRecognizer) {
         print("Add Reference Point")
-        
+        if hasBeenPlaced { return }
         let tapLocation = recognizer.location(in: sceneView)
         guard let query = sceneView.raycastQuery(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal) else { return  }
         let results = sceneView.session.raycast(query)
         guard let result = results.first else { return }
         let translation = result.worldTransform.columns
         
-//        let referencePointScene = SCNScene(named: "assetcatalog.scnassets/ReferencePoint.scn")
-//        guard let referencePointNode = referencePointScene?.rootNode.childNode(withName: "refPointNode", recursively: false) else { return }
-//
-//        let shader = "float u = _surface.diffuseTexcoord.x; \n" +
-//                    "float v = _surface.diffuseTexcoord.y; \n" +
-//                    "int u100 = int(u * 100); \n" +
-//                    "int v100 = int(v * 100); \n" +
-//                    "if (u100 % 99 == 0 || v100 % 99 == 0) { \n" +
-//                    "  // do nothing \n" +
-//                    "} else { \n" +
-//                    "    discard_fragment(); \n" +
-//                    "} \n"
-//
-//        referencePointNode.geometry?.firstMaterial?.emission.contents = UIColor.yellow
-//        referencePointNode.geometry?.firstMaterial?.shaderModifiers = [SCNShaderModifierEntryPoint.surface: shader]
-//        referencePointNode.geometry?.firstMaterial?.isDoubleSided = true
+        // MARK : 3D Ref Point
+        //        let referencePointScene = SCNScene(named: "assetcatalog.scnassets/ReferencePoint.scn")
+        //        guard let referencePointNode = referencePointScene?.rootNode.childNode(withName: "refPointNode", recursively: false) else { return }
+        //
+        //        let shader = "float u = _surface.diffuseTexcoord.x; \n" +
+        //                    "float v = _surface.diffuseTexcoord.y; \n" +
+        //                    "int u100 = int(u * 100); \n" +
+        //                    "int v100 = int(v * 100); \n" +
+        //                    "if (u100 % 99 == 0 || v100 % 99 == 0) { \n" +
+        //                    "  // do nothing \n" +
+        //                    "} else { \n" +
+        //                    "    discard_fragment(); \n" +
+        //                    "} \n"
+        //
+        //        referencePointNode.geometry?.firstMaterial?.emission.contents = UIColor.yellow
+        //        referencePointNode.geometry?.firstMaterial?.shaderModifiers = [SCNShaderModifierEntryPoint.surface: shader]
+        //        referencePointNode.geometry?.firstMaterial?.isDoubleSided = true
+                
+        //        referencePointNode.position = SCNVector3(translation.3.x, translation.3.y, translation.3.z)
+        //        sceneView.scene.rootNode.addChildNode(referencePointNode)
         
+        // MARK : PLANAR 2D Ref Point
         let image = UIImage(named: "RefPoint")
-        let refPointNode = SCNNode(geometry: SCNPlane(width: 0.1, height: 0.1))
-        refPointNode.position.z = 0.1
-        refPointNode.eulerAngles.x = -.pi / 2
-        refPointNode.geometry?.materials.first?.diffuse.contents  = image
-        refPointNode.position = SCNVector3(translation.3.x, translation.3.y, translation.3.z)
-        sceneView.scene.rootNode.addChildNode(refPointNode)
+        referencePoint = SCNNode(geometry: SCNPlane(width: 0.1, height: 0.1))
+        referencePoint.position.z = 0.1
+        referencePoint.eulerAngles.x = -.pi / 2
+        referencePoint.geometry?.materials.first?.diffuse.contents  = image
+        referencePoint.opacity = 0.7
+        referencePoint.position = SCNVector3(translation.3.x, translation.3.y, translation.3.z)
+        sceneView.scene.rootNode.addChildNode(referencePoint)
         
-//        referencePointNode.position = SCNVector3(translation.3.x, translation.3.y, translation.3.z)
-//        sceneView.scene.rootNode.addChildNode(referencePointNode)
+        
         hasBeenPlaced = true
     }
     
@@ -107,7 +125,7 @@ extension ARCameraViewController: ARSCNViewDelegate {
         let width = CGFloat(planeAnchor.extent.x)
         let height = CGFloat(planeAnchor.extent.z)
         let plane = SCNPlane(width: width, height: height)
-        plane.materials.first?.diffuse.contents = UIColor(hue: 255, saturation: 255, brightness: 255, alpha: 0.5)
+        plane.materials.first?.diffuse.contents = UIColor(hue: 255, saturation: 255, brightness: 255, alpha: 0)
 
         let planeNode = SCNNode(geometry: plane)
         planeNode.position = SCNVector3(CGFloat(planeAnchor.center.x), CGFloat(planeAnchor.center.y), CGFloat(planeAnchor.center.z))
@@ -116,8 +134,8 @@ extension ARCameraViewController: ARSCNViewDelegate {
         
 //        let refPointNode = addReferencePointer()
 //        planeNode.addChildNode(refPointNode)
-        
         node.addChildNode(planeNode)
+    
     }
     
     
